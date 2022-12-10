5c2ef77ec7360f03b360224482466f41a4fa3d8a
SharathKumarGitha <sharathgitha1627@gmail.com>
2022-12-10 21:57:11 +0530

    Demo2.java file added pls check man

---
+++ SharathDec10Project2022/src/com/dec10/Demo2.java
@@ -0,0 +1,9 @@
+package com.dec10;
+
+public class Demo2 {
+  public static void main(String[] args) {
+	
+	  System.out.println("this is first...");
+	  System.out.println("this is second....");
+}
+}
